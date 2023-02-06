bc6fb25851969af79d6a3895c602113f5f5fb912
Vivek Kumar Gupta <Vivek.K.Gupta@wolterskluwer.com>
2023-02-06 19:21:56 +0530

    first commit

---
+++ Models/MockCategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories => new List<Category>
+        {
+            new Category{CategoryId=1, CategoryName="Fruit Pies", Description="All-fruity-Pies"},
+            new Category{CategoryId=2, CategoryName="Cheese cakes", Description="Cheesy all the way"},
+            new Category{CategoryId=3, CategoryName="Seasonal pies", Description="Get in the mood for a seasonal pie"}
+        };
+
+        //public IEnumerable<Category> AllCategories() 
+        //{
+        //    return new List<Category>
+        //                            {
+        //                                new Category{CategoryId=1, CategoryName="Fruit Pies", Description="All-fruity-Pies"},
+        //                                new Category{CategoryId=1, CategoryName="Fruit Pies", Description="All-fruity-Pies"},
+        //                                new Category{CategoryId=1, CategoryName="Fruit Pies", Description="All-fruity-Pies"}
+        //                            };
+        //}
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,69 @@
+using BethanyPieShop.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+
+            services.AddScoped<IPieRepository, PieRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+
+            services.AddHttpContextAccessor();
+            services.AddSession();
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();  // when requires to run the app only on https
+            app.UseStaticFiles();   // A middleware in asp.net for running static files like images, JS, css files from wwwroot folder in asp.net
+            app.UseSession();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
